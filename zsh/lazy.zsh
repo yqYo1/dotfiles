3fee3ad9c3638f3eaeb7f96a6659b6952f7ec19a
yqYo1 <miyasyrog@gmail.com>
2024-02-26 23:35:02 +0900

    add jj

--- zsh/lazy.zsh
+++ zsh/lazy.zsh
@@ -3,3 +3,4 @@ alias cat=bat
 alias vi=nvim
 gh completion -s zsh >> $ZSHRC_DIR/completion/gh
 rye self completion -s zsh > $ZSHRC_DIR/completion/rye
+jj util completion --zsh > $ZSHRC_DIR/completion/jj
