6f6ea7a5cf3a75198f92e219bdba42dae844cec1
yqYo1 <miyasyrog@gmail.com>
2024-03-03 18:12:50 +0900

    add full_visual_line

--- nvim/lua/cli/cli.lua
+++ nvim/lua/cli/cli.lua
@@ -3,28 +3,5 @@ return {
 	dir = "",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		-- ruby
-		--{ "Shopify/ruby-lsp", version = "*", build = { "gem install --user-install --no-document --pre ruby-lsp" } },
-
-		-- zig
-		{
-			"zigtools/zls",
-			build = {
-				"bun x @oven/zig build -Doptimize=ReleaseSafe --verbose",
-			},
-			config = function(spec)
-				local Path = require("plenary.path")
-
-				local dir = spec.dir
-
-				local BIN_DIR = Path:new(dir, "zig-out", "bin")
-
-				if not (BIN_DIR:exists()) then
-					BIN_DIR:mkdir({ parents = true })
-				end
-
-				vim.env.PATH = BIN_DIR:absolute() .. ":" .. vim.env.PATH
-			end,
-		},
 	},
 }
