b024e093eb1afe1cd6f64fb0f1af2de3359abbd3
yqYo1 <miyasyrog@gmail.com>
2024-03-12 14:40:17 +0900

    aqua update.sh

---
+++ aquaproj-aqua/update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eu
+AQUA_DIR="$(dirname $AQUA_GLOBAL_CONFIG)"
+cd $AQUA_DIR
+set +e
+log=$AQUA_DIR/log.txt
+exec &> >(awk '{print strftime("[%Y/%m/%d %H:%M:%S] "),$0 } { fflush() } ' >> $log)
+aqua up
+aqua i -a
