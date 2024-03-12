6d58214af2b0e75cdd2b3761fb20601f2f73730c
yqYo1 <miyasyrog@gmail.com>
2024-03-12 18:10:53 +0900

    python tools

--- nvim/lua/cli/cli.lua
+++ nvim/lua/cli/cli.lua
@@ -1,6 +1,7 @@
 return {
 	name = "cli",
 	dir = "",
+  enabled = false,
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
--- nvim/lua/cli/python_tools/init.lua
+++ nvim/lua/cli/python_tools/init.lua
@@ -2,12 +2,10 @@ return {
 	name = "python_tools",
 	dir = vim.fn.fnamemodify(debug.getinfo(1).source:sub(2), ":h"),
 	dependencies = { "nvim-lua/plenary.nvim" },
-	build = "rye sync",
+	build = "rye sync --update-all",
 	config = function(spec)
 		local Path = require("plenary.path")
-
 		local BIN_DIR = Path:new(spec.dir, ".venv", "bin"):absolute()
-
 		vim.env.PATH = BIN_DIR .. ":" .. vim.env.PATH
 	end,
 }
