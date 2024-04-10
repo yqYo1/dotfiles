4a55a1d8d2e0ea4ef0498bd68e1792c23a0dd9cb
yqYo1 <miyasyrog@gmail.com>
2024-04-10 21:25:56 +0900

    update nvim config

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -1,3 +1,4 @@
 require("config.global")
 require("config.options")
+require("config.filetype")
 require("config.lazy")
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,48 +1,52 @@
-vim.opt.backup = true
-vim.opt.backupdir = vim.fn.expand(vim.fn.stdpath("cache") .. "/.vim_backup")
-vim.opt.swapfile = false
-vim.opt.writebackup = true
-vim.opt.autoread = true
-vim.opt.hidden = true
-vim.opt.mouse = "a"
-vim.opt.confirm = true
+local opt = vim.opt
 
-vim.opt.shortmess:append("I")
+opt.backup = true
+opt.backupdir = vim.fn.expand(vim.fn.stdpath("cache") .. "/.vim_backup")
+opt.swapfile = false
+opt.writebackup = true
+opt.autoread = true
+opt.hidden = true
+opt.mouse = "a"
+opt.confirm = true
 
-vim.opt.number = true
-vim.opt.relativenumber = true
+opt.shortmess:append("I")
 
-vim.opt.fenc = "utf-8"
+opt.number = true
+opt.relativenumber = true
+
+opt.fenc = "utf-8"
 
 local tabwidth = 2
-vim.opt.tabstop = tabwidth
-vim.opt.softtabstop = tabwidth
-vim.opt.shiftwidth = tabwidth
-vim.opt.expandtab = true
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.backspace = { "indent", "eol", "start" }
-vim.opt.nrformats:remove({ "unsigned", "octal" })
+opt.tabstop = tabwidth
+opt.softtabstop = tabwidth
+opt.shiftwidth = tabwidth
+opt.expandtab = true
+opt.autoindent = true
+opt.smartindent = true
+opt.backspace = { "indent", "eol", "start" }
+opt.nrformats:remove({ "unsigned", "octal" })
 
-vim.opt.list = true
+opt.list = true
 
-vim.opt.listchars = {
-	tab = "▸▹┊",
-	trail = "▫",
-	extends = "❯",
-	precedes = "❮",
+opt.listchars = {
+  tab = "▸▹┊",
+  trail = "▫",
+  extends = "❯",
+  precedes = "❮",
 }
-vim.opt.pumblend = 10
---vim.opt.laststatus = 0
+opt.pumblend = 10
+
+vim.diagnostic.config({ severity_sort = true })
+--opt.laststatus = 0
 
 --[[
-local opt = vim.opt
+local opt = opt
 
 opt.title = true
 
 -- 全角文字表示設定
 if not vim.g.vscode then
-	opt.ambiwidth = "double"
+  opt.ambiwidth = "double"
 end
 
 opt.termguicolors = true
