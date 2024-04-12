ee70888c52ddbd212680db3dc573244a38b072d3
yqYo1 <miyasyrog@gmail.com>
2024-04-12 23:33:36 +0900

    add noice.nvim

---
+++ nvim/lua/plugin/noice.lua
@@ -0,0 +1,46 @@
+local has = require("core.plugin").has
+
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  cond = not is_vscode(),
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+  },
+  init = function()
+    require("plugin.telescope").le("noice")
+  end,
+  opts = function()
+    return {
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      popupmenu = {
+        enabled = true,
+        backend = has("nvim-cmp") and "cmp" or "nui",
+      },
+      notify = {
+        enabled = true,
+        view = "mini",
+      },
+      cmdline = {
+        enabled = true,
+      },
+      messages = {
+        view_search = "virtualtext",
+        view = "mini",
+      },
+      presets = {
+        inc_rename = true,
+        lsp_doc_border = true,
+        long_message_to_split = true,
+      },
+    }
+  end,
+  config = true,
+}
---
+++ nvim/lua/plugin/telescope.lua
@@ -0,0 +1,36 @@
+local on_load = require("core.plugin").on_load
+
+local M = {}
+
+--@param name string
+local le = function(name)
+  on_load("telescope.nvim", function()
+    require("telescope").load_extension(name)
+  end)
+end
+
+M = {
+  "nvim-telescope/telescope.nvim",
+  cond = not is_vscode(),
+  cmd = { "Telescope" },
+  dependencies = {
+    { "nvim-lua/plenary.nvim" },
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make", config = le("fzf") },
+    { "nvim-telescope/telescope-live-grep-args.nvim", config = le("live_grep_args") },
+    { "nvim-telescope/telescope-symbols.nvim" },
+    { "nvim-telescope/telescope-github.nvim", config = le("gh") },
+    { "tsakirist/telescope-lazy.nvim", config = le("lazy") },
+  },
+  init = function()
+    vim.api.nvim_create_autocmd("BufReadPost", {
+      pattern = "*",
+      callback = function()
+        vim.cmd.stopinsert()
+      end,
+    })
+  end,
+}
+
+M.le = le
+
+return M
