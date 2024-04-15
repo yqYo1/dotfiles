04c0992910f66df7f44c81336ad3959b94bd7f9a
yayoi <miyasrog@gmail.com>
2024-04-15 14:47:07 +0900

    use pwsh

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,3 @@
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole("Administrators")) { Start-Process powershell.exe "-File `"$PSCommandPath`"" -Verb RunAs; exit }
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole("Administrators")) { Start-Process pwsh.exe "-File `"$PSCommandPath`"" -Verb RunAs; exit }
 
 
