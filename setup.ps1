27bf9ba2f0961576e9ec71d89f00b7d1aa1c267f
yayoi <miyasrog@gmail.com>
2024-04-15 14:36:49 +0900

    windows

---
+++ setup.ps1
@@ -0,0 +1,3 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole("Administrators")) { Start-Process powershell.exe "-File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+
