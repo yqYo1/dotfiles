239047db48b4b8ca4ce74df4250d4c32599d6aae
yqYo1 <miyasyrog@gmail.com>
2024-04-17 23:41:32 +0900

    update setup.ps1

--- setupScripts/BuildToolsSetup.ps1
+++ setupScripts/BuildToolsSetup.ps1
@@ -4,10 +4,9 @@ if (
     [Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()
   ).IsInRole("Administrators")){
   Start-Process pwsh.exe "-File `"$PSCommandPath`"" -Verb RunAs -Wait
-  # . "$env:USERPROFILE\Documents\PowerShell\Profile.ps1"
 }else{
   Invoke-webRequest "https://aka.ms/vs/17/release/vs_BuildTools.exe" -OutFile "vs_BuildTools.exe"
-  Start-Process vs_BuildTools.exe "--passive --config `"$PSScriptRoot\setupScripts\minimum.vsconfig`"" -Wait
+  Start-Process vs_BuildTools.exe "--passive --config `"$PSScriptRoot\minimum.vsconfig`"" -Wait
   Remove-Item .\vs_BuildTools.exe
 }
 #pause
--- setupScripts/setup.ps1
+++ setupScripts/setup.ps1
@@ -1,27 +1,38 @@
+Push-Location -Path $PSScriptRoot
 # run as admin
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole("Administrators")) {
   Start-Process pwsh.exe "-File `"$PSCommandPath`"" -Verb RunAs -Wait
-    . "$env:USERPROFILE\Documents\PowerShell\Profile.ps1"
+  . "$env:USERPROFILE\Documents\PowerShell\Profile.ps1"
 }else{
-  winget install --id Kitware.CMake
-
   function makeDir($dir){
     if ( -not (Test-Path $dir)){
       New-Item $dir -ItemType Directory
     }
   }
-
-  $pwshDir = "$env:USERPROFILE\Documents\PowerShell"
-    makeDir $pwshDir
-    $pwshProfile = "$pwshDir\Profile.ps1"
-    if (Test-Path $pwshProfile) {
-      if ((Get-ItemProperty $pwshProfile).Mode.Substring(0,1) -ne 'l'){
-        Remove-Item $pwshProfile -Force
-          New-Item -ItemType SymbolicLink -Path $pwshProfile -Value "$PSScriptRoot\PowerShell\Profile.ps1"
+  function makeSymbolickLink($destination, $source){
+    if (Test-Path $destination) {
+      if ((Get-ItemProperty $destination).Mode.Substring(0,1) -ne 'l'){
+        Remove-Item $destination -Force  -Recurse
+        New-Item -ItemType SymbolicLink -Path $destination -Value "$source"
       }
     }else {
-      New-Item -ItemType SymbolicLink -Path $pwshProfile -Value "$PSScriptRoot\PowerShell\Profile.ps1"
+      New-Item -ItemType SymbolicLink -Path $destination -Value "$source"
     }
+  }
+
+  #install MSVC
+  . ./BuildToolsSetup.ps1
+  #install CMake
+  winget install --id Kitware.CMake
+
+  $dotConfig = "$env:USERPROFILE/.config"
+  makeDir $dotConfig
+  makeSymbolickLink "$dotConfig/starship.toml" "$PSScriptRoot/../starship.toml"
+
+  $pwshDir = "$env:USERPROFILE\Documents\PowerShell"
+  makeDir $pwshDir
+  $pwshProfile = "$pwshDir\Profile.ps1"
+  makeSymbolickLink $pwshProfile "$PSScriptRoot\..\PowerShell\Profile.ps1"
   . "$env:USERPROFILE\Documents\PowerShell\Profile.ps1"
 
     Get-Command aqua -ea SilentlyContinue | Out-Null
@@ -32,24 +43,12 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
     }
 
   $nvimDir = "$env:USERPROFILE\AppData\Local\nvim"
-    makeDir $nvimDir
-    $InitLua = "$nvimDir\init.lua"
-    $nvimLuaDir = "$nvimDir\lua"
-    if (Test-Path $InitLua) {
-      if ((Get-ItemProperty $InitLua).Mode.Substring(0,1) -ne 'l'){
-        Remove-Item $InitLua -Force
-          New-Item -ItemType SymbolicLink -Path $InitLua -Value "$PSScriptRoot\nvim\init.lua"
-      }
-    }else {
-      New-Item -ItemType SymbolicLink -Path $InitLua -Value "$PSScriptRoot\nvim\init.lua"
-    }
-  if (Test-Path $nvimLuaDir) {
-    if ((Get-ItemProperty $nvimLuaDir).Mode.Substring(0,1) -ne 'l'){
-      Remove-Item $nvimLuaDir -Force  -Recurse
-        New-Item -ItemType SymbolicLink -Path $nvimLuaDir -Value "$PSScriptRoot\nvim\lua"
-    }
-  }else {
-    New-Item -ItemType SymbolicLink -Path $nvimLuaDir -Value "$PSScriptRoot\nvim\lua"
-  }
+  makeDir $nvimDir
+  $InitLua = "$nvimDir\init.lua"
+  $nvimLuaDir = "$nvimDir\lua"
+  makeSymbolickLink $InitLua "$PSScriptRoot/../nvim/init.lua"
+  makeSymbolickLink $nvimLuaDir "$PSScriptRoot/../nvim/lua"
+
   pause
 }
+Pop-Location
