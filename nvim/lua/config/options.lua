42ee7b75a1791bbc7501370f17b610f13b64b6c8
yqYo1 <miyasyrog@gmail.com>
2024-04-19 22:35:54 +0900

    nvim terminal on Windows change to pwsh

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -37,3 +37,18 @@ opt.listchars = {
 opt.pumblend = 10
 
 vim.diagnostic.config({ severity_sort = true })
+
+if is_windows() then
+  local pwsh_options = {
+    shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
+    shellcmdflag = "-NoLogo -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+    shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+    shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+    shellquote = "",
+    shellxquote = "",
+  }
+
+  for k, v in pairs(pwsh_options) do
+    opt[k] = v
+  end
+end
