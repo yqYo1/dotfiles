a42faa2132895a9eddbfb456125dbf917b21a016
yqYo1 <miyasyrog@gmail.com>
2024-04-24 14:01:25 +0900

    update wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,6 +6,9 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
+config.window_background_opacity = 0.75
 config.color_scheme = "Tokyo Night"
+config.font = wezterm.font("HackGen Console NF", { weight = "Regular", stretch = "Normal", style = "Normal" })
+config.font_size = 12
 
 return config
