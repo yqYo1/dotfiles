92f8b8aca1b1c75d4ed0f28cf3e043c5b9ff9f09
yqYo1 <miyasyrog@gmail.com>
2024-04-25 15:57:20 +0900

    update wezterm setting

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,10 +6,21 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-config.window_background_opacity = 0.75
+config.window_background_opacity = 0.60
 config.color_scheme = "Tokyo Night"
-config.font = wezterm.font("HackGen Console NF", { weight = "Regular", stretch = "Normal", style = "Normal" })
+config.font = wezterm.font_with_fallback({
+  {
+    family = "HackGen Console NF",
+    weight = "Regular",
+    stretch = "Normal",
+    style = "Normal",
+  },
+})
 config.font_size = 12
 config.adjust_window_size_when_changing_font_size = true
+
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+  config.default_prog = { "pwsh.exe", "-NoLogo" }
+end
 
 return config
