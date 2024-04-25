2320bfbd496a38ff4df0baf9c7689eb134992172
yqYo1 <miyasyrog@gmail.com>
2024-04-25 16:59:59 +0900

    update wezterm setting

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,8 +6,11 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
+--theme
 config.window_background_opacity = 0.60
 config.color_scheme = "Tokyo Night"
+
+--font
 config.font = wezterm.font_with_fallback({
   {
     family = "HackGen Console NF",
@@ -18,9 +21,23 @@ config.font = wezterm.font_with_fallback({
 })
 config.font_size = 12
 config.adjust_window_size_when_changing_font_size = true
+config.anti_alias_custom_block_glyphs = true
+config.bold_brightens_ansi_colors = "BrightOnly"
+--config.bold_brightens_ansi_colors = "No"
 
+--os
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
   config.default_prog = { "pwsh.exe", "-NoLogo" }
 end
+
+--gpu
+config.front_end = "WebGpu"
+
+config.audible_bell = "Disabled"
+config.use_ime = true
+
+config.animation_fps = 60
+
+config.disable_default_key_bindings = true
 
 return config
