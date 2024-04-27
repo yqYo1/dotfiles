b0b5bd8b37f8836217d37f5e19de326ef1831233
yqYo1 <miyasyrog@gmail.com>
2024-04-27 22:30:14 +0900

    update wezterm seting

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,8 +7,10 @@ if wezterm.config_builder then
 end
 
 --theme
-config.window_background_opacity = 0.70
 config.color_scheme = "Tokyo Night"
+
+--disable title bar
+config.window_decorations = "RESIZE"
 
 --font
 config.font = wezterm.font_with_fallback({
@@ -23,15 +25,19 @@ config.font_size = 12
 config.adjust_window_size_when_changing_font_size = true
 config.anti_alias_custom_block_glyphs = true
 --config.bold_brightens_ansi_colors = "BrightOnly"
-config.bold_brightens_ansi_colors = "No"
+--config.bold_brightens_ansi_colors = "No"
 
 --os
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
   config.default_prog = { "pwsh.exe", "-NoLogo" }
+  config.window_background_opacity = 0
+  config.win32_system_backdrop = "Acrylic"
 
   --todo: front_end set WebGpu only when using Intel Iris Xe Graphics
   --config.front_end = "WebGpu"
   config.front_end = "OpenGL"
+else
+  config.window_background_opacity = 0.70
 end
 
 config.audible_bell = "Disabled"
