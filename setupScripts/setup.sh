3408c710db02ce8f8a06239abd50db575091732d
yqYoi <miyasyrog@gmail.com>
2024-05-01 04:03:25 +0900

    fix setup.sh

--- setupScripts/setup.sh
+++ setupScripts/setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+XDG_CONFIG_HOME="$HOME/.config"
 curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v3.0.0/aqua-installer | bash
 CURRENT=$(cd $(dirname $0);pwd)
-ln -sf $CURRENT/zsh $XDG_CONFIG_HOME/zsh
-ln -sf $CURRENT/zsh/.zshrc $HOME/.zshrc
-ln -sf $CURRENT/starship.toml $XDG_CONFIG_HOME/starship.toml
-ln -sf $CURRENT/nvim $XDG_CONFIG_HOME/nvim
-ln -sf $CURRENT/aquaproj-aqua $XDG_CONFIG_HOME/aquaproj-aqua
-ln -sf $CURRENT/rye $XDG_CONFIG_HOME/rye
-ln -sf $CURRENT/bun/package.json $HOME/package.json
-ln -sf $CURRENT/bun/bun.lockb $HOME/bun.lockb
+ln -sfnvi $CURRENT/../zsh $XDG_CONFIG_HOME/zsh
+ln -sfnvi $CURRENT/../zsh/.zshrc $HOME/.zshrc
+ln -sfnvi $CURRENT/../starship.toml $XDG_CONFIG_HOME/starship.toml
+ln -sfnvi $CURRENT/../nvim $XDG_CONFIG_HOME/nvim
+ln -sfnvi $CURRENT/../aquaproj-aqua $XDG_CONFIG_HOME/aquaproj-aqua
+ln -sfnvi $CURRENT/../rye $XDG_CONFIG_HOME/rye
+#ln -sfnvi $CURRENT/../bun/package.json $HOME/package.json
+#ln -sfnvi $CURRENT/../bun/bun.lockb $HOME/bun.lockb
