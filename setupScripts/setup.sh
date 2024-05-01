89be2b30c38e9a37f5711fecce2b4422ef102076
yqYoi <miyasyrog@gmail.com>
2024-05-02 02:49:34 +0900

    fix zsh config

--- setupScripts/setup.sh
+++ setupScripts/setup.sh
@@ -4,9 +4,11 @@ curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v3.0.0/aqua
 CURRENT=$(cd $(dirname $0);pwd)
 ln -sfnvi $CURRENT/../zsh $XDG_CONFIG_HOME/zsh
 ln -sfnvi $CURRENT/../zsh/.zshrc $HOME/.zshrc
+ln -sfnvi $CURRENT/../zsh/sheldon $XDG_CONFIG_HOME/sheldon
 ln -sfnvi $CURRENT/../starship.toml $XDG_CONFIG_HOME/starship.toml
 ln -sfnvi $CURRENT/../nvim $XDG_CONFIG_HOME/nvim
 ln -sfnvi $CURRENT/../aquaproj-aqua $XDG_CONFIG_HOME/aquaproj-aqua
 ln -sfnvi $CURRENT/../rye $XDG_CONFIG_HOME/rye
 #ln -sfnvi $CURRENT/../bun/package.json $HOME/package.json
 #ln -sfnvi $CURRENT/../bun/bun.lockb $HOME/bun.lockb
+curl https://sh.rustup.rs -sSf | sh
--- zsh/lazy.zsh
+++ zsh/lazy.zsh
@@ -4,3 +4,4 @@ source <(gh completion -s zsh)
 source <(rye self completion -s zsh)
 source <(jj util completion --zsh)
 aqua update-aqua
+export PATH="$HOME/.local/bin:$PATH"
