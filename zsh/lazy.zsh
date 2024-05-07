e734e2641157fcaa76fbbfb2c21518929cadef3c
yqYoi <miyasyrog@gmail.com>
2024-05-08 00:33:45 +0900

    add alias ll on zsh

--- zsh/lazy.zsh
+++ zsh/lazy.zsh
@@ -1,7 +1,11 @@
+export FPATH="$ZSHRC_DIR/completion:$HOME/repos/eza/completions/zsh:$FPATH"
+autoload -Uz compinit && compinit
 export PATH="$HOME/.local/bin:$PATH"
 alias ls=eza
+alias ll='eza -alhF --git --git-repos'
 alias vi=nvim
 alias cls=clear
+export AQUA_PROGRESS_BAR=true
 source <(gh completion -s zsh)
 source <(rye self completion -s zsh)
 source <(jj util completion --zsh)
