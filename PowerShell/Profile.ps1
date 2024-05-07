1669f6e7403ab2d6b18d99c776dbf651a55c4116
yqYo1 <miyasyrog@gmail.com>
2024-05-07 20:54:29 +0900

    add alias ll on powershell

--- PowerShell/Profile.ps1
+++ PowerShell/Profile.ps1
@@ -1,5 +1,7 @@
-﻿Set-Alias ls eza
-Set-Alias vi nvim
+﻿Set-Alias -Name ls -Value eza
+Function ll {eza -alhF --git --git-repos}
+#Set-Alias -Name ll -Value eza
+Set-Alias -Name vi -Value nvim
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
