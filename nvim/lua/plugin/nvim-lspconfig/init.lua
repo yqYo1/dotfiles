b676f9ad77f45ffb547e149927b56b7ee3133523
yqYo1 <miyasyrog@gmail.com>
2024-05-09 23:09:28 +0900

    remove unnecessary comment

--- nvim/lua/plugin/nvim-lspconfig/init.lua
+++ nvim/lua/plugin/nvim-lspconfig/init.lua
@@ -182,29 +182,6 @@ return {
       },
     })
 
-    --[[
-    setup(lspconfig.yamlls, {
-      settings = {
-        yaml = {
-          schemas = require("schemastore").yaml.schemas({
-            extra = {
-              {
-                name = "openAPI 3.0",
-                url = "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.0/schema.yaml",
-              },
-              {
-                name = "openAPI 3.1",
-                url = "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.yaml",
-              },
-            },
-          }),
-          validate = true,
-          format = { enable = true },
-        },
-      },
-    })
-    ]]
-
     local python_lsp_init = function(_, config)
       config.settings.python.pythonPath = vim.env.VIRTUAL_ENV
           and lspconfig.util.path.join(vim.env.VIRTUAL_ENV, "bin", "python3")
