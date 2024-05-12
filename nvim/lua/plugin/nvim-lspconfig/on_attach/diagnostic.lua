7bd3866bff748977ec7a2878c64459139fbc372e
yqYo1 <miyasyrog@gmail.com>
2024-05-12 22:05:15 +0900

    add lsp diagnostic config

---
+++ nvim/lua/plugin/nvim-lspconfig/on_attach/diagnostic.lua
@@ -0,0 +1,29 @@
+local function diagnostic_formatter(diagnostic)
+  return string.format("[%s] %s (%s)", diagnostic.message, diagnostic.source, diagnostic.code)
+end
+
+return {
+  --on_attach = function(client, bufnr)
+  on_attach = function(_, _)
+    -- setup diagnostic signs
+    local signs = {
+      Error = " ",
+      Warn = " ",
+      Info = " ",
+      Hint = " ",
+    }
+    for type, icon in pairs(signs) do
+      local hl = "DiagnosticSign" .. type
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+    end
+
+    vim.diagnostic.config({
+      underline = true,
+      signs = true,
+      update_in_insert = false,
+      severity_sort = true,
+      virtual_text = false,
+      float = { sformat = diagnostic_formatter },
+    })
+  end,
+}
--- nvim/lua/plugin/nvim-lspconfig/on_attach/init.lua
+++ nvim/lua/plugin/nvim-lspconfig/on_attach/init.lua
@@ -1,6 +1,7 @@
----@type OnAttachCallback
 local on_attach = function(client, bufnr)
+  require("plugin.nvim-lspconfig.on_attach.diagnostic").on_attach(client, bufnr)
   require("plugin.nvim-lspconfig.on_attach.format").on_attach(client, bufnr)
+  --require("plugin.nvim-lspconfig.on_attach.inlayhints").on_attach(client, bufnr)
 end
 
 return on_attach
