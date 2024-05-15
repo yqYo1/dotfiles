ad5b71a7805f89667c58910e4e4e00b43656d528
yqYo1 <miyasyrog@gmail.com>
2024-05-16 02:45:29 +0900

    update python_tools

--- nvim/lua/cli/python_tools/init.lua
+++ nvim/lua/cli/python_tools/init.lua
@@ -1,5 +1,22 @@
 return {
   name = "python_tools",
+  dir = "",
+  opts = {
+    tools = {
+      "ruff",
+      "ruff-lsp",
+      "pylyzer",
+    },
+  },
+  config = function(_, opts)
+    --[[
+    vim.schedule(function()
+      vim.iter(opts.tools):map(_l("x: vim.system({ 'rye', 'install', '-f', x }, { text = true })"))
+    end)
+    ]]
+  end,
+
+  --[[
   dir = vim.fn.fnamemodify(debug.getinfo(1).source:sub(2), ":h"),
   dependencies = { "nvim-lua/plenary.nvim" },
   build = "rye sync --update-all",
@@ -8,4 +25,5 @@ return {
     local BIN_DIR = Path:new(spec.dir, ".venv", "bin"):absolute()
     vim.env.PATH = BIN_DIR .. ":" .. vim.env.PATH
   end,
+  ]]
 }
