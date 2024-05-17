e1c60e6a1b21d2efe595458c3d415ab1d91fd38e
yqYoi <miyasyrog@gmail.com>
2024-05-17 14:45:27 +0900

    python

--- nvim/lua/plugin/nvim-lspconfig/servers/python.lua
+++ nvim/lua/plugin/nvim-lspconfig/servers/python.lua
@@ -4,17 +4,10 @@ local lspconfig = require("lspconfig")
 local setup = lsp_utils.setup
 
 --[[
-local pythonName = "python3"
-
-if is_windows() then
-  pythonName = "python"
-end
-
-local function python_lsp_init(_, config)
-  local lspconfig = require("lspconfig")
+local python_lsp_init = function(_, config)
   config.settings.python.pythonPath = vim.env.VIRTUAL_ENV
-      and lspconfig.util.path.join(vim.env.VIRTUAL_ENV, "bin", pythonName)
-    or utils.find_cmd(pythonName, ".venv/bin", config.root_dir)
+      and lspconfig.util.path.join(vim.env.VIRTUAL_ENV, "bin", "python3")
+    or utils.find_cmd("python3", ".venv/bin", config.root_dir)
 end
 ]]
 
@@ -25,34 +18,6 @@ local python_lsp_init = function(_, config)
 end
 
 return {
-  {
-    name = "pylyzer",
-    dir = "",
-    enabled = false,
-    dependencies = {
-      "neovim/nvim-lspconfig",
-      "python_tools",
-    },
-    ft = function(spec)
-      return lsp_utils.get_default_filetypes(spec.name)
-    end,
-    opts = function()
-      return {
-        --before_init = python_lsp_init,
-        settings = {
-          python = {
-            checkOnType = false,
-            diagnostics = true,
-            inlayHints = true,
-            smartCompletion = true,
-          },
-        },
-      }
-    end,
-    config = function(spec, opts)
-      setup(spec.name, opts)
-    end,
-  },
   {
     name = "basedpyright",
     dir = "",
@@ -65,7 +30,7 @@ return {
     end,
     opts = function()
       return {
-        --before_init = python_lsp_init,
+        before_init = python_lsp_init,
         settings = {
           basedpyright = {
             disableOrganizeImports = false,
@@ -89,6 +54,7 @@ return {
   {
     name = "ruff_lsp",
     dir = "",
+    enabled = false,
     dependencies = {
       "neovim/nvim-lspconfig",
       "python_tools",
@@ -96,19 +62,16 @@ return {
     ft = function(spec)
       return lsp_utils.get_default_filetypes(spec.name)
     end,
-    opts = function()
-      return {
-        --before_init = python_lsp_init,
+    config = function(spec, _)
+      setup(spec.name, {
+        before_init = python_lsp_init,
         init_options = {
           settings = {
             -- Any extra CLI arguments for `ruff` go here.
             args = {},
           },
         },
-      }
-    end,
-    config = function(spec, opts)
-      setup(spec.name, opts)
+      })
     end,
   },
 }
