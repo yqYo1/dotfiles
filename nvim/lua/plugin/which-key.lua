5b46e3694e721e70a4aacf905cbc038fbfac8f6f
yqYo1 <miyasyrog@gmail.com>
2024-05-31 11:34:23 +0900

    format file

--- nvim/lua/plugin/which-key.lua
+++ nvim/lua/plugin/which-key.lua
@@ -1,13 +1,13 @@
 return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
+  "folke/which-key.nvim",
+  event = "VeryLazy",
   enabled = false,
-	config = function()
-		require("which-key").setup({
-			plugins = {
-				registers = false,
-			},
-			operators = { gc = "Comments" },
-		})
-	end,
+  config = function()
+    require("which-key").setup({
+      plugins = {
+        registers = false,
+      },
+      operators = { gc = "Comments" },
+    })
+  end,
 }
