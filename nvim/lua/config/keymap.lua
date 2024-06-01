46ca96533fcd3decdc700ba5653edd845e51568f
yqYo1 <miyasyrog@gmail.com>
2024-06-01 23:43:15 +0900

    add comment

--- nvim/lua/config/keymap.lua
+++ nvim/lua/config/keymap.lua
@@ -3,5 +3,6 @@ vim.keymap.set({ "n", "v" }, "gy", '"+y')
 vim.keymap.set("n", "gp", '"+p')
 vim.keymap.set("n", "gP", '"+P')
 
+-- Buffer switch
 vim.keymap.set("n", "<C-j>", "<cmd>bprev<CR>")
 vim.keymap.set("n", "<C-k>", "<cmd>bnext<CR>")
