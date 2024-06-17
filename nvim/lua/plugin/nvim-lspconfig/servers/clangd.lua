163a203bf4739ee3e300e562a4da8ad6e690792c
yqYo1 <miyasyrog@gmail.com>
2024-06-17 09:54:08 +0900

    add clangd

---
+++ nvim/lua/plugin/nvim-lspconfig/servers/clangd.lua
@@ -0,0 +1,20 @@
+local lsp_utils = require("plugin.nvim-lspconfig.uitls")
+local setup = lsp_utils.setup
+local format_config = lsp_utils.format_config
+
+return {
+  name = "clangd",
+  dir = "",
+  dependencies = { "neovim/nvim-lspconfig" },
+  ft = function(spec)
+    return lsp_utils.get_default_filetypes(spec.name)
+  end,
+  opts = function()
+    return {
+      settings = {},
+    }
+  end,
+  config = function(spec, opts)
+    setup(spec.name, opts)
+  end,
+}
