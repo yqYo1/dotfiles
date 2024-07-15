cbc1d803d42cf4667a04047e4059263f28b105bf
yqYo1 <miyasyrog@gmail.com>
2024-07-15 11:11:09 +0900

    use TEMP dir

--- nvim/lua/plugin/nvim-lspconfig/servers/clangd.lua
+++ nvim/lua/plugin/nvim-lspconfig/servers/clangd.lua
@@ -1,10 +1,16 @@
 local lsp_utils = require("plugin.nvim-lspconfig.uitls")
 local setup = lsp_utils.setup
 local format_config = lsp_utils.format_config
+local dir_name
+if is_windows() then
+  dir_name = vim.env.TEMP .. "\\nvim\\lsp-clangd"
+else
+  dir_name = vim.env.TMPDIR .. "/lsp-clangd"
+end
 
 return {
   name = "clangd",
-  dir = "",
+  dir = dir_name,
   dependencies = { "neovim/nvim-lspconfig" },
   ft = function(spec)
     return lsp_utils.get_default_filetypes(spec.name)
