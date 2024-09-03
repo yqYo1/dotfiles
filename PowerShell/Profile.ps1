85371fda079c299705db09d683d126b41da6d5bb
yqYo1 <miyasyrog@gmail.com>
2024-09-03 18:38:14 +0900

    add OSC 7 on PowerShell

--- PowerShell/Profile.ps1
+++ PowerShell/Profile.ps1
@@ -25,3 +25,13 @@ if(Test-Path $apikeyps1){
 Remove-Variable apikeyps1
 
 Invoke-Expression (&starship init powershell)
+$prompt = ""
+function Invoke-Starship-PreCommand {
+    $current_location = $executionContext.SessionState.Path.CurrentLocation
+    if ($current_location.Provider.Name -eq "FileSystem") {
+        $ansi_escape = [char]27
+        $provider_path = $current_location.ProviderPath -replace "\\", "/"
+        $prompt = "$ansi_escape]7;file://${env:COMPUTERNAME}/${provider_path}$ansi_escape\"
+    }
+    $host.ui.Write($prompt)
+}
