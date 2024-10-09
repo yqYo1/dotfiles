4fd3ba8e67bdea4f5d27917095ed7d3314c2c628
yqYo1 <miyasyrog@gmail.com>
2024-10-09 15:40:36 +0900

    remove blank lines

--- nvim/lua/cli/python_tools/init.lua
+++ nvim/lua/cli/python_tools/init.lua
@@ -5,9 +5,7 @@ return {
   build = "rye sync",
   config = function(spec)
     local Path = require("plenary.path")
-
     local BIN_DIR = Path:new(spec.dir, ".venv", "bin"):absolute()
-
     vim.env.PATH = BIN_DIR .. ":" .. vim.env.PATH
   end,
 }
