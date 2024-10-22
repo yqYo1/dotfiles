0f5feb7ae44277ad94eb2d1ba56205c4338ed17a
yqYoi <miyasyrog@gmail.com>
2024-10-22 19:21:12 +0900

    update init

--- aquaproj-aqua/update.sh
+++ aquaproj-aqua/update.sh
@@ -5,5 +5,5 @@ cd $AQUA_DIR
 set +e
 log=$AQUA_DIR/log.txt
 #exec &> >(awk '{print strftime("[%Y/%m/%d %H:%M:%S] "),$0 } { fflush() } ' >> $log)
-aqua upa 2>&1 | tee -a $log
 aqua i -a -l 2>&1 | tee -a $log
+aqua upa 2>&1 | tee -a $log
