19b12a4f1b182fc28d2d5f7c008a6b41e33188e2
yqYoi <miyasyrog@gmail.com>
2024-11-02 02:29:57 +0900

    update keymap on neovim

--- nvim/lua/config/keymap.lua
+++ nvim/lua/config/keymap.lua
@@ -6,3 +6,13 @@ vim.keymap.set("n", "gP", '"+P')
 -- Buffer switch
 vim.keymap.set("n", "<C-j>", "<cmd>bprev<CR>")
 vim.keymap.set("n", "<C-k>", "<cmd>bnext<CR>")
+
+-- Terminal escape insert mode
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+
+-- Make sure Undo Block is not interrupted
+vim.keymap.set("i", "<Left>", "<C-G>U<Left>")
+vim.keymap.set("i", "<Right>", "<C-G>U<Right>")
+
+-- redo
+vim.keymap.set("n", "U", "<C-r>")
