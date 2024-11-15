61c6ddcaa78e4107a2a57d232ce104c192660f1c
yqYo1 <miyasyrog@gmail.com>
2024-11-16 01:37:29 +0900

    add copilotchat

---
+++ nvim/lua/plugin/copilotchat.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    event = "VeryLazy",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    -- build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+}
