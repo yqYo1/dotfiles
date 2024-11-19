105a7cbe4a7c6d35c8dbafec533bbeeb99f2402e
yqYoi <miyasyrog@gmail.com>
2024-11-20 03:49:24 +0900

    add sum plugin

--- emacs/init.el
+++ emacs/init.el
@@ -15,3 +15,14 @@
 (leaf leaf-convert
   :doc "Convert many format to leaf format"
   :ensure t)
+
+(leaf autorevert
+  :doc "revert buffers when files on disk change"
+  :global-minor-mode global-auto-revert-mode)
+(leaf which-key
+  :doc "Display available keybindings in popup"
+  :ensure t
+  :global-minor-mode t)
+(leaf paren
+  :doc "highlight matching paren"
+  :global-minor-mode show-paren-mode)
