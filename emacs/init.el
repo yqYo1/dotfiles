e8cd22ae2d8db80a4306dd04e8e9379f5921dc7f
yqYoi <miyasyrog@gmail.com>
2024-11-20 03:44:43 +0900

    add init.el

---
+++ emacs/init.el
@@ -0,0 +1,17 @@
+(eval-and-compile
+  (customize-set-variable
+   'package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                       ("melpa" . "https://melpa.org/packages/")))
+  (package-initialize)
+  (use-package leaf :ensure t)
+
+  (leaf leaf-keywords
+    :ensure t
+    :init
+    (leaf blackout :ensure t)
+    :config
+    (leaf-keywords-init)))
+
+(leaf leaf-convert
+  :doc "Convert many format to leaf format"
+  :ensure t)
