5f990a103676be6298bb4950516510c98f01e9e2
yqYo1 <miyasyrog@gmail.com>
2024-11-21 14:22:02 +0900

    update config lazygit

--- nvim/lua/plugin/lsp_lines.lua
+++ nvim/lua/plugin/lsp_lines.lua
@@ -2,20 +2,6 @@
 return {
   "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
   name = "lsp_lines",
-  dependencies = { "neovim/nvim-lspconfig" },
   event = { "LspAttach" },
-  -- keys = {
-  --   {
-  --     "<Leader>l",
-  --     function()
-  --       require("lsp_lines").toggle()
-  --     end,
-  --     mode = { "n", "v" },
-  --     desc = "Toggle lsp_lines",
-  --   },
-  -- },
   opts = {},
-  -- config = function()
-  --   require("lsp_lines").setup()
-  -- end,
 }
