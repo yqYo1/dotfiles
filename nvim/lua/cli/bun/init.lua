5a20e289a0c3123e24854cc26e6d40eb97045eb9
yqYo1 <miyasyrog@gmail.com>
2024-12-20 10:56:35 +0900

    add bun

---
+++ nvim/lua/cli/bun/init.lua
@@ -0,0 +1,14 @@
+return {
+  name = "bun_tools",
+  dir = vim.fn.fnamemodify(debug.getinfo(1).source:sub(2), ":h"),
+  build = "bun install",
+  config = function(spec)
+    local dir = spec.dir
+
+    vim.system({ "bun", "install" }, { cwd = dir, text = true }, _l("obj: vim.print(obj.stdout)"))
+
+    local BIN_DIR =
+      assert((vim.system({ "bun", "pm", "bin" }, { cwd = dir, text = true }):wait()).stdout):gsub("\n[^\n]*$", "")
+    vim.env.PATH = ("%s:%s"):format(BIN_DIR, vim.env.PATH)
+  end,
+}
