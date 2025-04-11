43adef5921f7a39c1af537d31f97968d714173d0
yqYoi <miyasyrog@gmail.com>
2025-04-12 05:27:14 +0900

    fix githook
    poset-mergeがlinuxで2回リンクされる問題を修正

--- git-hook/post-merge-main.sh
+++ git-hook/post-merge-main.sh
@@ -1,7 +1,8 @@
 DOTDIR=$(cd $(dirname $0)/../;pwd)
-ln -sfnv $DOTDIR/git-hook/post-merge $DOTDIR/.git/hooks/post-merge
 if [ $(uname -o) = "GNU/Linux" ]; then
   $DOTDIR/setupScripts/setup.sh
 elif [ $(uname -o) = "Msys" ]; then
+  # Linuxはsetup.shでリンクする為不要
+  ln -sfnv $DOTDIR/git-hook/post-merge $DOTDIR/.git/hooks/post-merge
   $DOTDIR/aquaproj-aqua/update.sh
 fi
