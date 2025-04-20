d6bb629ca5f1bf46ad0b1c3c4c7f3ada4a4d84bf
yqYo1 <miyasyrog@gmail.com>
2025-04-21 00:44:19 +0900

    fix LuaSnip

--- nvim/lua/plugin/LuaSnip.lua
+++ nvim/lua/plugin/LuaSnip.lua
@@ -8,6 +8,7 @@ return {
   config = function()
     require("luasnip").setup({})
     require("luasnip.loaders.from_snipmate").lazy_load()
+    require("luasnip.loaders.from_vscode").lazy_load()
     require("luasnip.loaders.from_lua").lazy_load()
   end,
 }
