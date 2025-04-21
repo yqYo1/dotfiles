94c4d3378d661038f05ec4681775939d313163a5
yqYo1 <miyasyrog@gmail.com>
2025-04-21 15:23:28 +0900

    add memo

--- nvim/lua/plugin/LuaSnip.lua
+++ nvim/lua/plugin/LuaSnip.lua
@@ -1,7 +1,14 @@
+-- local build_cmd = "make install_jsregexp"
+-- if require("core.utils").is_windows() then
+--   build_cmd = 'sh -c "' .. build_cmd .. 'CC=gcc.exe SHELL=C:/Program Files/Git/bin/sh.exe .SHELLFLAGS=-c"'
+--   -- vim.print(build_cmd)
+-- end
+
 ---@type LazySpec
 return {
   "L3MON4D3/LuaSnip",
   version = "*",
+  -- build = build_cmd,
   dependencies = {
     "rafamadriz/friendly-snippets",
   },
