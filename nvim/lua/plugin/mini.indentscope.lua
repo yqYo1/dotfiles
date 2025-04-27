ae97991d9efedf90885aaa4bba47c8698d5de1b2
yqYo1 <miyasyrog@gmail.com>
2025-04-28 00:15:00 +0900

    add memo

--- nvim/lua/plugin/mini.indentscope.lua
+++ nvim/lua/plugin/mini.indentscope.lua
@@ -6,6 +6,8 @@ return {
   opts = {},
   config = function(_, opts)
     require('mini.indentscope').setup(opts)
+    -- disable draw indentscope
+    -- due to use indent-blankline.nvim
     vim.g.miniindentscope_disable = true
   end,
 }
