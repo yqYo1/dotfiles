1be89841daa08c599729aeadee75091a699dbeb2
yqYo1 <miyasyrog@gmail.com>
2025-04-30 23:54:51 +0900

    update nvim-autopairs

--- nvim/lua/plugin/nvim-autopairs.lua
+++ nvim/lua/plugin/nvim-autopairs.lua
@@ -1,18 +1,26 @@
+
+local function has_cmp()
+  return require("core.plugin").has("nvim-cmp")
+end
+
 ---@type LazySpec
 return {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    opts = {},
-    config = function(_, opts)
-      require("nvim-autopairs").setup(opts)
-      local has_cmp = require("core.plugin").has("nvim-cmp")
-      if has_cmp then
-        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-        local cmp = require("cmp")
-        cmp.event:on(
+  'windwp/nvim-autopairs',
+  event = "InsertEnter",
+  -- cond = function()
+  --   return has_cmp()
+  -- end,
+  cond = has_cmp(),
+  opts = {},
+  config = function(_, opts)
+    require("nvim-autopairs").setup(opts)
+    if has_cmp() then
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      local cmp = require("cmp")
+      cmp.event:on(
         "confirm_done",
         cmp_autopairs.on_confirm_done()
-        )
-      end
+      )
     end
+  end
 }
