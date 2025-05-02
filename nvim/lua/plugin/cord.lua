d167ca15d9bb119232b6c5750de6f44c40e07351
yqYo1 <miyasyrog@gmail.com>
2025-05-03 00:26:42 +0900

    update cord.nvim config

--- nvim/lua/plugin/cord.lua
+++ nvim/lua/plugin/cord.lua
@@ -3,7 +3,13 @@ return {
   "vyfor/cord.nvim",
   build = ':Cord update',
   event = "VeryLazy",
+  ---@module "cord"
+  ---@type CordConfig
+  ---@diagnostic disable-next-line:missing-fields
   opts = {
+    display = {
+      flavor = "accent",
+    },
     idle = {
       enabled = false,
     },
