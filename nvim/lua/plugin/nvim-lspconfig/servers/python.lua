7536ba01a661b50e04dbdc0e2d8566af6c9fb8ac
yqYoi <miyasyrog@gmail.com>
2025-05-07 05:08:37 +0900

    fix python lsp config

--- nvim/lua/plugin/nvim-lspconfig/servers/python.lua
+++ nvim/lua/plugin/nvim-lspconfig/servers/python.lua
@@ -1,9 +1,10 @@
 local lsp_utils = require("plugin.nvim-lspconfig.uitls")
+local is_windows = require("core.utils").is_windows()
 local setup = lsp_utils.setup
 local utils = require("core.utils")
 local lspconfig = require("lspconfig")
 local python_lsp_init = function(_, config)
-  if is_windows() then
+  if is_windows then
     config.settings.python.pythonPath = vim.env.VIRTUAL_ENV
         and lspconfig.util.path.join(vim.env.VIRTUAL_ENV, "Scripts", "python")
       or utils.find_cmd("python.exe", ".venv/Scripts", config.root_dir)
