dfd3c32487ab7184c9ac2b6fc33866ba744a39d0
yqYoi <miyasyrog@gmail.com>
2025-05-15 16:20:01 +0900

    change coler LineNr

--- nvim/lua/plugin/catppuccin.lua
+++ nvim/lua/plugin/catppuccin.lua
@@ -36,10 +36,13 @@ return {
       properties = {},
       types = {},
       operators = {},
-      -- miscs = {}, -- Uncomment to turn off hard-coded styles
     },
     color_overrides = {},
-    custom_highlights = {},
+    custom_highlights = function(colors)
+      return {
+        LineNr = { fg = colors.overlay1 },
+      }
+    end,
     default_integrations = true,
     integrations = {
       cmp = false,
