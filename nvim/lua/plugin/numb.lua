da344a186aff631f719c83293d8b1f175252148c
yqYo1 <miyasyrog@gmail.com>
2025-05-20 16:17:15 +0900

    add numb.nvim

---
+++ nvim/lua/plugin/numb.lua
@@ -0,0 +1,12 @@
+---@type LazySpec
+return {
+  "nacro90/numb.nvim",
+  event = "VeryLazy",
+  opts = {
+    show_numbers = true,
+    show_cursorline = true,
+    hide_relativenumbers = true,
+    number_only = false,
+    centered_peeking = true,
+  }
+}
