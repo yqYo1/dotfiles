e985ffb5e481da605141248e519b6427e372a45f
yqYoi <miyasyrog@gmail.com>
2025-05-25 02:35:50 +0900

    add outline.nvim

---
+++ nvim/lua/plugin/outline.lua
@@ -0,0 +1,12 @@
+---@type LazySpec
+return {
+  "hedyhli/outline.nvim",
+  lazy = true,
+  cmd = { "Outline", "OutlineOpen" },
+  keys = {
+    { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+  },
+  opts = {
+    -- Your setup opts here
+  },
+}
