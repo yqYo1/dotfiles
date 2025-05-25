7b3392eca1067e64122014128e65ce2a010f79d3
yqYoi <miyasyrog@gmail.com>
2025-05-25 21:41:51 +0900

    update ccc.nvim config

--- nvim/lua/plugin/ccc.lua
+++ nvim/lua/plugin/ccc.lua
@@ -1,13 +1,31 @@
 local is_vscode = require("core.utils").is_vscode
 
+---@type LazySpec
 return {
   "uga-rosa/ccc.nvim",
   cond = not is_vscode(),
   event = { "VeryLazy" },
-  opts = {
-    highlighter = {
-      auto_enable = true,
-      lsp = true,
-    },
-  },
+  config = function()
+    local ccc = require("ccc")
+    ccc.setup({
+      highlighter = {
+        auto_enable = true,
+        lsp = true,
+      },
+      pickers = {
+        ccc.picker.ansi_escape(),
+        ccc.picker.hex,
+        ccc.picker.hex_long,
+        ccc.picker.hex_short,
+        ccc.picker.css_rgb,
+        ccc.picker.css_hsl,
+        ccc.picker.css_hwb,
+        ccc.picker.css_lab,
+        ccc.picker.css_lch,
+        ccc.picker.css_oklab,
+        ccc.picker.css_oklch,
+        ccc.picker.css_name,
+      },
+    })
+  end,
 }
