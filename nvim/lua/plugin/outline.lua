3f94f876f5edf26b97827b7f3ca9d81879b135b7
yqYo1 <miyasyrog@gmail.com>
2025-05-27 23:25:55 +0900

    update outline config

--- nvim/lua/plugin/outline.lua
+++ nvim/lua/plugin/outline.lua
@@ -1,12 +1,20 @@
 ---@type LazySpec
 return {
   "hedyhli/outline.nvim",
-  lazy = true,
-  cmd = { "Outline", "OutlineOpen" },
   keys = {
-    { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+    {
+      "<leader>o",
+      -- "<cmd>Outline<CR>",
+      function()
+        require("outline").toggle()
+      end,
+      desc = "Toggle outline"
+    },
   },
   opts = {
-    -- Your setup opts here
+    outline_window = {
+      position = "left",
+      show_numbers = true,
+    },
   },
 }
