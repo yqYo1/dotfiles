88195ed5f982e3794be26df512c8b6ea16bf0678
yqYo1 <miyasyrog@gmail.com>
2025-06-01 02:10:23 +0900

    fix

--- nvim/lua/plugin/in-and-out.lua
+++ nvim/lua/plugin/in-and-out.lua
@@ -1,22 +1,16 @@
 ---@type LazySpec
 return {
   "ysmb-wtsg/in-and-out.nvim",
-  --   enabled = true,
-  event = "InsertEnter",
-  -- keys = {
-  --   {
-  --     "<C-CR>",
-  --     function()
-  --       require("in-and-out").in_and_out()
-  --     end,
-  --     mode = "i"
-  --   },
-  -- },
+  -- event = "InsertEnter",
+  keys = {
+    {
+      "<C-o>",
+      function()
+        require("in-and-out").in_and_out()
+      end,
+      mode = "i",
+      desc = "in-and-out"
+    },
+  },
   opts = {},
-  config = function(_, opts)
-    require("in-and-out").setup(opts)
-    vim.keymap.set("i", "<C-CR>", function()
-      require("in-and-out").in_and_out()
-    end)
-  end,
 }
