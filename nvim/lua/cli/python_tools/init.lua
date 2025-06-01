025feb760bc2a29f6341390fce026281ea7dee8a
yqYo1 <miyasyrog@gmail.com>
2025-06-01 22:41:42 +0900

    fix

--- nvim/lua/cli/python_tools/init.lua
+++ nvim/lua/cli/python_tools/init.lua
@@ -2,7 +2,7 @@ return {
   name = "python_tools",
   dir = vim.fn.fnamemodify(debug.getinfo(1).source:sub(2), ":h"),
   dependencies = { "nvim-lua/plenary.nvim" },
-  build = "rye sync --update-all",
+  build = "rye sync",
   config = function(spec)
     local Path = require("plenary.path")
     local BIN_DIR = Path:new(spec.dir, ".venv", "bin"):absolute()
