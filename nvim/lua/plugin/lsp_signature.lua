d9c2ecd19d087103de83c2808f2ca551d036b6c8
yqYoi <miyasyrog@gmail.com>
2025-06-02 21:36:15 +0900

    lsp_signatrue.nvim config move from lsp.on_attach to plugin file

--- nvim/lua/plugin/lsp_signature.lua
+++ nvim/lua/plugin/lsp_signature.lua
@@ -2,5 +2,26 @@
 return {
   "ray-x/lsp_signature.nvim",
   event = { "LspAttach" },
-  opt = {},
+  opt = {
+    hint_enable = false, -- disable hint on virtual text
+    bind = true,
+    handler_opts = {
+      border = "rounded",
+    },
+    auto_close_after = 10,
+    floating_window_off_x = 5, -- adjust float windows x position.
+    floating_window_off_y = function() -- adjust float windows y position. e.g. set to -2 can make floating window move up 2 lines
+      -- local linenr = vim.api.nvim_win_get_cursor(0)[1] -- buf line number
+      local pumheight = vim.o.pumheight
+      local winline = vim.fn.winline() -- line number in the window
+      local winheight = vim.fn.winheight(0)
+
+      -- window top
+      if winline - 1 < pumheight then return pumheight end
+
+      -- window bottom
+      if winheight - winline < pumheight then return -pumheight end
+      return 0
+    end,
+  },
 }
--- nvim/lua/plugin/nvim-lspconfig/on_attach/init.lua
+++ nvim/lua/plugin/nvim-lspconfig/on_attach/init.lua
@@ -1,6 +1,6 @@
 local on_attach = function(client, bufnr)
   require("plugin.nvim-lspconfig.on_attach.format").on_attach(client, bufnr)
-  require("plugin.nvim-lspconfig.on_attach.signature").on_attach(client, bufnr)
+  -- require("plugin.nvim-lspconfig.on_attach.signature").on_attach(client, bufnr)
 end
 
 return on_attach
