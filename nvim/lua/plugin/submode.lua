17525db168f4c8309c6b0cf91967718c413624fc
yqYoi <miyasyrog@gmail.com>
2025-06-03 01:51:35 +0900

    add submode

---
+++ nvim/lua/plugin/submode.lua
@@ -0,0 +1,28 @@
+---@type LazySpec
+return {
+  "pogyomo/submode.nvim",
+  event = "VeryLazy",
+  config = function()
+    local submode = require("submode")
+    submode.create("WindowMoveAndResize", {
+      mode = "n",
+      enter = "<C-w>",
+      leave = { "q", "<ESC>" },
+      default = function(register)
+        register("h", "<C-w>h")
+        register("j", "<C-w>j")
+        register("k", "<C-w>k")
+        register("l", "<C-w>l")
+        register("<lt>", "<C-w><lt>")
+        register(">", "<C-w>>")
+        register("+", "<C-w>+")
+        register("=", "<C-w>=")
+        register("-", "<C-w>-")
+        register("H", "<C-w><lt>")
+        register("L", "<C-w>>")
+        register("J", "<C-w>+")
+        register("K", "<C-w>-")
+      end,
+    })
+  end,
+}
