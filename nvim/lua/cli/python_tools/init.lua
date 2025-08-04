5c1c376709464c233cbb005a99f71b7ca5e13716
yqYoi <miyasyrog@gmail.com>
2025-08-04 22:15:47 +0900

    migrate rye to uv

--- nvim/lua/cli/python_tools/init.lua
+++ nvim/lua/cli/python_tools/init.lua
@@ -2,7 +2,7 @@
 return {
   name = "python_tools",
   dir = vim.fn.fnamemodify(debug.getinfo(1).source:sub(2), ":h"),
-  build = "rye sync",
+  build = "uv sync",
   config = function(spec)
     local Path = require("plenary.path")
     local BIN_DIR = Path:new(spec.dir, ".venv", "bin"):absolute()
