2acb998206049c3b1da46881a6f2dc4d21485cb1
yqYoi <miyasyrog@gmail.com>
2026-02-24 02:54:26 +0900

    feat(cord): add catppuccin theme and text labels

--- nvim/lua/plugin/cord.lua
+++ nvim/lua/plugin/cord.lua
@@ -1,17 +1,24 @@
 ---@type LazySpec
 return {
   "vyfor/cord.nvim",
-  build = ':Cord update',
+  build = ":Cord update",
   event = "VeryLazy",
   ---@module "cord"
   ---@type CordConfig
   ---@diagnostic disable-next-line:missing-fields
   opts = {
     display = {
+      theme = "catppuccin",
       flavor = "accent",
     },
     idle = {
       enabled = false,
+    },
+    text = {
+      workspace = "",
+      editing = "Editing",
+      viewing = "Viewing",
+      file_browser = "Browsing files",
     },
   },
 }
