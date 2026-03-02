56d9529947d3eb992a98de4979a393e71f360565
yqYoi <miyasyrog@gmail.com>
2026-03-03 03:17:33 +0900

    chore(plugin): remove InsertEnter event and set lazy false

--- nvim/lua/plugin/nvim-treesitter-endwise.lua
+++ nvim/lua/plugin/nvim-treesitter-endwise.lua
@@ -1,13 +1,6 @@
 ---@type LazySpec
 return {
   "RRethy/nvim-treesitter-endwise",
-  event = "InsertEnter",
-  config = function()
-    ---@diagnostic disable-next-line: missing-fields
-    require("nvim-treesitter.configs").setup({
-      endwise = {
-        enable = true,
-      },
-    })
-  end
+  -- event = "InsertEnter",
+  lazy = false,
 }
