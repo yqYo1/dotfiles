653fabf78254496cd18b70cba4290f416da13d8a
yqYo1 <52419113+yqYo1@users.noreply.github.com>
2026-03-13 00:28:56 +0900

    feat(render-markdown): prevent loading in VSCode

--- nvim/lua/plugin/render-markdown.lua
+++ nvim/lua/plugin/render-markdown.lua
@@ -1,8 +1,10 @@
+local is_vscode = require("core.utils").is_vscode
+
 ---@type LazySpec
 return {
   'MeanderingProgrammer/render-markdown.nvim',
-  -- event = 'VeryLazy',
   ft = { "markdown", "markdown.mdx", "Avante", "codecompanion" },
+  cond = not is_vscode(),
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
     opts = {
