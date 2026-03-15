32214c9904ee1e3d0194115a3fb1fe0d41ec525a
yqYoi <52419113+yqYo1@users.noreply.github.com>
2026-03-15 22:36:29 +0900

    feat(git-hook): add interactive prompt to run-setup
    Add a read prompt in git-hook/run-setup.sh and a case handler.
    On yes, run existing uname-based commands:
      - setupScripts/setup.sh on GNU/Linux
      - link git-hook/post-merge and run aquaproj-aqua/update.sh on Msys
    On no, skip the update; other input prints "Invalid input."

--- git-hook/run-setup.sh
+++ git-hook/run-setup.sh
@@ -1,8 +1,20 @@
 DOTDIR=$(cd $(dirname $0)/../;pwd)
-if [ $(uname -o) = "GNU/Linux" ]; then
-  $DOTDIR/setupScripts/setup.sh
-elif [ $(uname -o) = "Msys" ]; then
-  # Linuxはsetup.shでリンクする為不要
-  ln -sfnv $DOTDIR/git-hook/post-merge $DOTDIR/.git/hooks/post-merge
-  $DOTDIR/aquaproj-aqua/update.sh
-fi
+read -p "Do you want to update environment? [y/N] " yn
+case "yn" in
+  [Yy]|[Yy][Ee][Ss])
+    echo "Updating environment..."
+    if [ $(uname -o) = "GNU/Linux" ]; then
+      $DOTDIR/setupScripts/setup.sh
+    elif [ $(uname -o) = "Msys" ]; then
+      # Linuxはsetup.shでリンクする為不要
+      ln -sfnv $DOTDIR/git-hook/post-merge $DOTDIR/.git/hooks/post-merge
+      $DOTDIR/aquaproj-aqua/update.sh
+    fi
+    ;;
+  [Nn]|[Nn][Oo]|"")
+    echo "Skipping environment update."
+    ;;
+  *)
+    echo "Invalid input."
+    ;;
+esac
