a159c5f7989166751e2b81084fcb403dbc0d3914
yqYo1 <52419113+yqYo1@users.noreply.github.com>
2026-03-20 23:07:27 +0900

    refactor(git-hook): disable prompt on Win

--- git-hook/run-setup.sh
+++ git-hook/run-setup.sh
@@ -1,20 +1,20 @@
 DOTDIR=$(cd $(dirname $0)/../;pwd)
-read -p "Do you want to update environment? [y/N] " yn
-case "yn" in
-  [Yy]|[Yy][Ee][Ss])
-    echo "Updating environment..."
-    if [ $(uname -o) = "GNU/Linux" ]; then
+if [ $(uname -o) = "GNU/Linux" ]; then
+  read -p "Do you want to update environment? [y/N] " yn
+  case "yn" in
+    [Yy]|[Yy][Ee][Ss])
+      echo "Updating environment..."
       $DOTDIR/setupScripts/setup.sh
-    elif [ $(uname -o) = "Msys" ]; then
-      # Linuxはsetup.shでリンクする為不要
-      ln -sfnv $DOTDIR/git-hook/post-merge $DOTDIR/.git/hooks/post-merge
-      $DOTDIR/aquaproj-aqua/update.sh
-    fi
-    ;;
-  [Nn]|[Nn][Oo]|"")
-    echo "Skipping environment update."
-    ;;
-  *)
-    echo "Invalid input."
-    ;;
-esac
+      ;;
+    [Nn]|[Nn][Oo]|"")
+      echo "Skipping environment update."
+      ;;
+    *)
+      echo "Invalid input."
+      ;;
+  esac
+elif [ $(uname -o) = "Msys" ]; then
+  # Linuxはsetup.shでリンクする為不要
+  ln -sfnv $DOTDIR/git-hook/post-merge $DOTDIR/.git/hooks/post-merge
+  $DOTDIR/aquaproj-aqua/update.sh
+fi
