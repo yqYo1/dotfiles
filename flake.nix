3f828f4242eed1ec9f43fac88dab1f03b017d232
yqYoi <52419113+yqYo1@users.noreply.github.com>
2026-04-01 14:25:34 +0900

    feat(flake): add multi-system support
    Import systems input (nix-systems/default-linux) in flake.nix
    Add mkPkgs and mkHomeConfiguration helpers and per-system apps
    Generate flake.homeConfigurations from the imported systems

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default-linux";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,62 +14,83 @@
 
   outputs =
     inputs@{
-      self,
       nixpkgs,
       flake-parts,
       home-manager,
+      systems,
       ...
     }:
     let
-      system = "x86_64-linux";
+      lib = nixpkgs.lib;
       username = "yayoi";
       homeDirectory = "/home/${username}";
-      pkgs = import nixpkgs {
-        inherit system;
-        config = {
-          allowUnfreePackages = [
-            "github-copilot-cli"
-          ];
-        };
-      };
+      linuxSystems = import systems;
 
-      mkHmApp =
-        name: command:
-        let
-          app = pkgs.writeShellApplication {
-            name = "hm-${name}";
-            runtimeInputs = [
-              home-manager.packages.${system}.home-manager
+      mkPkgs =
+        system:
+        import nixpkgs {
+          inherit system;
+          config.allowUnfreePredicate =
+            pkg:
+            builtins.elem (lib.getName pkg) [
+              "github-copilot-cli"
             ];
-            text = ''
-              exec home-manager ${command} "$@"
-            '';
+        };
+
+      mkHomeConfiguration =
+        system:
+        home-manager.lib.homeManagerConfiguration {
+          pkgs = mkPkgs system;
+
+          modules = [ ./home.nix ];
+
+          extraSpecialArgs = {
+            inherit username homeDirectory;
           };
-        in
-        {
-          type = "app";
-          program = "${app}/bin/hm-${name}";
         };
     in
-    {
-      apps.${system} = rec {
-        switch = mkHmApp "switch" "switch --flake .#${username}";
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = linuxSystems;
 
-        build = mkHmApp "build" "build --flake .#${username}";
+      perSystem =
+        { system, ... }:
+        let
+          pkgs = mkPkgs system;
+          hmAttr = "${username}-${system}";
 
-        generations = mkHmApp "generations" "generations --flake .#${username}";
-
-        default = switch;
-      };
-
-      homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-
-        modules = [ ./home.nix ];
-
-        extraSpecialArgs = {
-          inherit username homeDirectory;
+          mkHmApp =
+            name: command:
+            let
+              app = pkgs.writeShellApplication {
+                name = "hm-${name}";
+                runtimeInputs = [
+                  home-manager.packages.${system}.home-manager
+                ];
+                text = ''
+                  exec home-manager ${command} "$@"
+                '';
+              };
+            in
+            {
+              type = "app";
+              program = "${app}/bin/hm-${name}";
+            };
+        in
+        {
+          apps = rec {
+            switch = mkHmApp "switch" "switch --flake .#${hmAttr}";
+            build = mkHmApp "build" "build --flake .#${hmAttr}";
+            generations = mkHmApp "generations" "generations --flake .#${hmAttr}";
+            default = switch;
+          };
         };
-      };
+
+      flake.homeConfigurations = lib.genAttrs (map (system: "${username}-${system}") linuxSystems) (
+        name:
+        let
+          system = lib.removePrefix "${username}-" name;
+        in
+        mkHomeConfiguration system
+      );
     };
 }
